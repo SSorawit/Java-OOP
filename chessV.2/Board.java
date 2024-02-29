4bc590fb3d02b127c31b47081975948746b01455
S.Sorawit <sowarit007@gmail.com>
2024-02-29 11:55:18 +0700

    new chess

--- chessV.2/Board.java
+++ chessV.2/Board.java
@@ -1,3 +1,29 @@
 public class Board {
-
+    Figure brd[][] = new Figure[8][8];
+    public Board(){
+    }
+    void boardInitiate() {
+    }
+    void display() {
+        System.out.print("  ");
+        for (int i = 1; i < 9; i++) {
+            System.out.print("   ");
+            System.out.print(i);
+        }
+        System.out.println();
+        System.out.println("--------------------------------------------------------");
+        for (int i = 0; i < 8; i++) {
+            System.out.print(i + 1);
+            System.out.print(" |");
+            for (int j = 0; j < 8; j++) {
+                if (brd[i][j] != null) {
+                    System.out.printf("  %s ", brd[i][j].name);
+                } else
+                    System.out.print(" -- ");
+            }
+            System.out.print(" |");
+            System.out.println(" ");
+        }
+        System.out.println("--------------------------------------------------------");
+    }
 }
--- chessV.2/Figure.java
+++ chessV.2/Figure.java
@@ -1,3 +1,13 @@
 public class Figure {
+    public String name = "";
+    public int currentRow;
+    public int currentColumn;
+    public boolean isWhite; // trur is white flase is black
 
+    public Figure(String name,boolean isWhite,int row,int column) {
+        this.name = name;
+        this.isWhite = isWhite;
+        this.currentRow = row;
+        this.currentColumn = column;
+    }
 }
